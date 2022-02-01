eb9f104a55839a3c6fc16ba904061c991365be8f
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-31 20:24:04 -0600

    running test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,9 @@
 const Employee = require('../lib/Employee');
+
+test('create and Employee and show name id and email', () => {
+    const employeeInfo = new Employee('Mari', 960421, 'employeemail@here.com');
+
+    expect(employeeInfo.name).toBe('Mari');
+    expect(employeeInfo.id).toBe('960421');
+    expect(employeeInfo.email).toBe('employeemail@here.com');
+});
