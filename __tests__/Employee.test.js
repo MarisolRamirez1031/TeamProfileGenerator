05b48a1a20e16e20a244e34a652863e1d4594474
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-31 21:06:12 -0600

    employee test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,9 @@
 const Employee = require('../lib/Employee');
 
-test('create and Employee and show name id and email', () => {
-    const employeeInfo = new Employee('Mari', 960421, 'employeemail@here.com');
+test('create an employee object and show name id and email', () => {
+    const employeeInfo = new Employee('Mari', 960421, 'employeeemail@here.com');
 
     expect(employeeInfo.name).toBe('Mari');
-    expect(employeeInfo.id).toBe('960421');
-    expect(employeeInfo.email).toBe('employeemail@here.com');
+    expect(employeeInfo.id).toBe(960421);
+    expect(employeeInfo.email).toBe('employeeemail@here.com');
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,9 @@
 const Engineer = require('../lib/Engineer');
+
+test('create an engineer object and show name id and email', () => {
+    const engineer = new Engineer('Mari', 960421, 'employeeemail@here.com');
+
+    expect(engineer.name).toBe('Mari');
+    expect(engineer.id).toBe(960421);
+    expect(engineer.email).toBe('employeeemail@here.com');
+});
