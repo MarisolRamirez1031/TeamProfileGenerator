dd09b1b1ea49b5ab11349d09292513e5e5470551
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-31 21:27:06 -0600

    working on intern test, currently failed

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,10 @@
 const Intern = require('../lib/Intern');
+
+test('create an intern object and show name id email and school', () => {
+    const internData = new Intern('Mari', 960421, 'employeeemail@here.com', 'UT at Austin');
+
+    expect(internData.name).toBe('Mari');
+    expect(internData.id).toBe(960421);
+    expect(internData.email).toBe('employeeemail@here.com');
+    expect(internData.school).toBe('UT at Austin');
+})
