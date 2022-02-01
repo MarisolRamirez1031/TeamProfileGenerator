5302bf4a6b21157ea050b6e6d3b960477916d955
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-31 22:45:23 -0600

    manager test failed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,11 @@
 const Manager = require('../lib/Manager');
+
+test('create a Manager object', () => {
+    const managerData = new Manager('Mari', 960421, 'employeeemail@here.com');
+
+    expect(managerData.name).toBe('Mari');
+    expect(managerData.id).toBe(960421);
+    expect(managerData.email).toBe('employeeemail@here.com');
+    expect(managerData.officeNumber).toBe('1031');
+    expect(managerData.role).toBe('Manager');
+});
